d092b80033cb7ea6ef75fe1ad8fdd0e774bebc13
Janusz Chudzynski <janusz@izotx.com>
2016-07-09 14:31:06 -0500

    Initial Commit

---
+++ JMCFlexibleLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JMCFlexibleLayout
+//
+//  Created by Janusz Chudzynski on 7/9/16.
+//  Copyright © 2016 izotx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
